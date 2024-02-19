501f0e6cf33ce9108c777ec96ba16d72c9e57538
InsikDeveloper <129082367+InsikDeveloper@users.noreply.github.com>
2024-02-19 05:45:09 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let answer;
+
+    document.querySelector('#add').onclick = function(){
+        answer = parseInt(document.querySelector('#number1').value) + parseInt(document.querySelector('#number2').value);
+        document.querySelector('p').innerText = "Hello World!😃";
+    }
+
+    document.querySelector('#subtract').onclick = function(){
+        answer = parseInt(document.querySelector('#number1').value) - parseInt(document.querySelector('#number2').value);
+        document.querySelector('p').innerText = "Hello World!😃";
+    }
+
+    document.querySelector('#multiply').onclick = function(){
+        answer = parseInt(document.querySelector('#number1').value) * parseInt(document.querySelector('#number2').value);
+        document.querySelector('p').innerText = "Hello World!😃";
+    }
+
+    document.querySelector('#divide').onclick = function(){
+        answer = parseInt(document.querySelector('#number1').value) / parseInt(document.querySelector('#number2').value);
+        document.querySelector('p').innerText = "Hello World1😃";
+    }
+
+    document.querySelector('#clear').onclick = function(){
+        window.location.reload();
+    }
+
+
+
+// document.querySelector('button').onclick = function(){
+//     answer = int(document.querySelector('input').value);
+//     window.alert(`Hello, ${username}`);
+// }
